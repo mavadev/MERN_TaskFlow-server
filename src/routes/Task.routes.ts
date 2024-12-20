44cae9fda0e51c85c4adbe8dc2550472dd01de83
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2024-12-20 14:07:20 -0500

    route[POST]: añadir validación para crear la tarea

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { body, param } from 'express-validator';
 import { TaskController } from '../controllers/Task.controller';
 import { validateProjectExists } from '../middleware/project';
+import { handleValidationErrors } from '../middleware/validation';
 
 const router = Router();
 
-router.route('/:projectID/tasks')
-	.post(validateProjectExists, TaskController.createTask);
+router
+	.route('/:projectID/tasks')
+	.post(
+		param('projectID').isMongoId().withMessage('ID de proyecto no válido'),
+		body('name').trim().notEmpty().withMessage('El Nombre de la Tarea es Obligatorio'),
+		body('description').trim().notEmpty().withMessage('La Descripción de la Tarea es Obligatoria'),
+		handleValidationErrors,
+		validateProjectExists,
+		TaskController.createTask
+	);
 
 export default router;
