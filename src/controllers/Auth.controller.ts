b27ba8748d4b0d641251a1e577e200c428abd026
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2025-01-16 14:59:44 -0500

    route[POST]: generar token al crear cuenta de usuario

--- src/controllers/Auth.controller.ts
+++ src/controllers/Auth.controller.ts
@@ -1,24 +1,29 @@
 import type { Request, Response } from 'express';
 import User from '../models/User.model';
+import Token from '../models/Token.model';
 import { hashPassword } from '../utils/auth';
+import { generateToken } from '../utils/token';
 
 export class AuthController {
 	static createAccount = async (req: Request, res: Response) => {
 		try {
 			const { email, password } = req.body;
-			const user = new User(req.body);
 
-			// Encriptar password
-			user.password = await hashPassword(password);
-
-			// Evitar usuarios duplicados
-			const userExists = await User.findOne({ email });
-			if (userExists) {
+			// Verificar si el usuario existe
+			const existingUser = await User.findOne({ email });
+			if (existingUser) {
 				res.status(409).json({ error: 'El usuario ya existe' });
 				return;
 			}
 
-			await user.save();
+			// Crear y guardar usuario
+			const hashedPassword = await hashPassword(password);
+			const user = await User.create({ ...req.body, password: hashedPassword });
+
+			// Generar y guardar token
+			const token = generateToken();
+			await Token.create({ token, user: user._id });
+
 			res.status(201).json({ message: 'Cuenta creada correctamente', data: user });
 		} catch (error) {
 			res.status(500).json({ error: 'Error al crear la cuenta' });
---
+++ src/models/Token.model.ts
@@ -0,0 +1,26 @@
+import { Document, Schema, model } from 'mongoose';
+
+interface IToken extends Document {
+	token: string;
+	user: Schema.Types.ObjectId;
+	createdAt: Date;
+}
+
+const tokenSchema = new Schema({
+	token: {
+		type: String,
+		required: true,
+	},
+	user: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		ref: 'User',
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+		expires: '10m',
+	},
+});
+
+export default model<IToken>('Token', tokenSchema);
